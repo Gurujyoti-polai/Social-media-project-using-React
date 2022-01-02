0d48629cee846a85e93c147ae6bdf5384b9b5ce1
Gurujyoti-polai <79266786+Gurujyoti-polai@users.noreply.github.com>
2022-01-02 15:40:09 +0530

    Redirecting the logged in user

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
+import { Navigate } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-import { login } from '../actions/auth';
+import { login, clearAuthState } from '../actions/auth';
 
 class Login extends Component {
   constructor(props) {
@@ -12,6 +13,9 @@ class Login extends Component {
       email: '',
       password: '',
     };
+  }
+  componentWillUnmount() {
+    this.props.dispatch(clearAuthState());
   }
 
   handleEmailChange = (e) => {
@@ -39,7 +43,11 @@ class Login extends Component {
   };
 
   render() {
-    const { error, inProgress } = this.props.auth;
+    const { error, inProgress, isLoggedin } = this.props.auth;
+
+    if (isLoggedin) {
+      return <Navigate to="/" />;
+    }
     return (
       <form className="login-form">
         <span className="login-signup-header">Log In</span>
